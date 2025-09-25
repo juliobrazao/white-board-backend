5f4153dccbd1d07a646df15c39e5d8dcdb0ac593
Julio Brazao <julio@brazao.dev>
2025-09-24 20:25:11 -0400

    Migration setup

---
+++ src/infra/database/migrations/1758759773845-create-orders.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateOrders1758759773845 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
